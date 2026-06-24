b4b22046e6e11af50ff5f706ec87eeb10cb90135
JLHC123 <jlhc3751@gmail.com>
2026-06-23 21:57:04 -0700

    added the controllers, showdialog, alertdialog, so that you can add new foods to the list.
    But due to it unable to use MaterialApp properly, the app had to be reognized completely (MaterialApp can't be in Stateful).

--- FoodApp/food_app/lib/main.dart
+++ FoodApp/food_app/lib/main.dart
@@ -4,67 +4,113 @@ void main() {
   runApp(const FoodApp());
 }
 
-class FoodApp extends StatefulWidget {
+class FoodApp extends StatelessWidget {
   const FoodApp({super.key});
-
-  @override
-  State<FoodApp> createState() => _FoodApp();
-}
-
-class _FoodApp extends State<FoodApp> {
-  final foods = [
-  {'name': 'Milk', 'expirationDate': '2026-06-15'},
-  {'name': 'Eggs', 'expirationDate': '2026-06-20'},
-  {'name': 'Cheese', 'expirationDate': '2026-06-25'},
-];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Food Expiration App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Food Expiration App'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Welcome to the Food Expiration App!',
-              ),
-              const SizedBox(height: 16.0),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: foods.length,
-                  itemBuilder: (context, index) {
-                    final food = foods[index];
-                    return ListTile(
-                      title: Text(food['name']!),
-                      subtitle: Text('Expires: ${food['expirationDate']}'),
-                      onTap: () {
-                        print('${food['name']} tapped');
+      home: const FoodHomePage(),
+    );
+  }
+}
+
+class FoodHomePage extends StatefulWidget {
+  const FoodHomePage({super.key});
+
+  @override
+  State<FoodHomePage> createState() => _FoodHomePageState();
+}
+
+class _FoodHomePageState extends State<FoodHomePage> {
+  final foods = [
+    {'name': 'Milk', 'expirationDate': '2026-06-15'},
+    {'name': 'Eggs', 'expirationDate': '2026-06-20'},
+    {'name': 'Cheese', 'expirationDate': '2026-06-25'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Food Expiration App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Welcome to the Food Expiration App!',
+            ),
+            const SizedBox(height: 16.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: foods.length,
+                itemBuilder: (context, index) {
+                  final food = foods[index];
+                  return ListTile(
+                    title: Text(food['name']!),
+                    subtitle: Text('Expires: ${food['expirationDate']}'),
+                    onTap: () {
+                      print('${food['name']} tapped');
+                    },
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        showDeleteDialog(context, food, index);
                       },
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          showDeleteDialog(context, food, index);
-                        },
-                      )
-                    );
-                  }
-                )
+                    )
+                  );
+                }
               )
-            ]
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Add Food');
-          },
-          child: const Icon(Icons.add),
-        ),
+            )
+          ]
+        )
+      ),
+
+      
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final foodNameController = TextEditingController(); 
+          final expirationDateController = TextEditingController();
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Add Food'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: foodNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Food Name',
+                    ),
+                  ),
+                  TextField(
+                    controller: expirationDateController,
+                    decoration: const InputDecoration(
+                      labelText: 'Expiration Date',
+                    ),
+                  )
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      foods.add({'name': foodNameController.text, 'expirationDate': expirationDateController.text});
+                    });
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Add'),
+                )
+              ],
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
