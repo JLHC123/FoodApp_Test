a45fa7fb7710e4f40b3806abd0634ce6c7180cb8
JLHC123 <jlhc3751@gmail.com>
2026-06-23 17:39:25 -0700

    Added ListTile and then ListView.builder, will make a new function for the onTap:

--- FoodApp/food_app/lib/main.dart
+++ FoodApp/food_app/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+final foods = [
+  {'name': 'Milk', 'expirationDate': '2026-06-15'},
+  {'name': 'Eggs', 'expirationDate': '2026-06-20'},
+  {'name': 'Cheese', 'expirationDate': '2026-06-25'},
+];
+
 void main() {
   runApp(const FoodApp());
 }
@@ -24,9 +30,51 @@ class FoodApp extends StatelessWidget {
                 'Welcome to the Food Expiration App!',
               ),
               const SizedBox(height: 16.0),
-              const Text('Milk - Expires: 2026-06-15'),
-              const Text('Eggs - Expires: 2026-06-20'),
-              const Text('Cheese - Expires: 2026-06-25'),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: foods.length,
+                  itemBuilder: (context, index) {
+                    final food = foods[index];
+                    return ListTile(
+                      title: Text(food['name']!),
+                      subtitle: Text('Expires: ${food['expirationDate']}'),
+                      onTap: () {
+                        print('${food['name']} tapped');
+                      },
+                    );
+                  }
+                )
+              )
+              // Expanded(
+              //   child: ListView(
+              //     children: [
+              //       ListTile(
+              //         title: Text('Milk'),
+              //         subtitle: Text('Expires: 2026-06-15'),
+              //         onTap: () {
+              //           print('Milk tapped');
+              //         }
+              //       ),
+              //       ListTile(
+              //         title: Text('Eggs'),
+              //         subtitle: Text('Expires: 2026-06-20'),
+              //         onTap: () {
+              //           print('Eggs tapped');
+              //         }
+              //       ),
+              //       ListTile(
+              //         title: Text('Cheese'),
+              //         subtitle: Text('Expires: 2026-06-25'),
+              //         onTap: () {
+              //           print('Cheese tapped');
+              //         }
+              //       ),
+              //     ]
+              //   )
+              // )
+              // const Text('Milk - Expires: 2026-06-15'),
+              // const Text('Eggs - Expires: 2026-06-20'),
+              // const Text('Cheese - Expires: 2026-06-25'),
             ]
           )
         )
