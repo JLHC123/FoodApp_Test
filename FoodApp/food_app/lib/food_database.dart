cd3a997559bddd6a444d3486e64c1b8ae0879e57
JLHC123 <jlhc3751@gmail.com>
2026-07-02 19:22:29 -0700

    We create getAllFoods, insertFood, initalizeDatabase, deleteFood, and then we use them in main.dart and now the code works with sql!
    Possible steps I might need to take is to make things more cleaner (factoring) or adding some more features.

--- FoodApp/food_app/lib/food_database.dart
+++ FoodApp/food_app/lib/food_database.dart
@@ -1,2 +1,58 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'food.dart';
+
 class FoodDatabase {
+    Database? _database;
+
+    // initalize the database
+    Future<void> initalizeDatabase() async {
+        final databasePath = await getDatabasesPath();
+        // Create new database seperate from the original food_app.db in case we need to continue testing in the original python code
+        final path = join(databasePath, 'food_expiration_database.db');
+
+        _database = await openDatabase(
+            path,
+            version: 1,
+            onCreate: (db, version) async {
+                await db.execute('''
+                    CREATE TABLE food(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL,
+                        expirationDate TEXT NOT NULL
+                    )
+                ''');
+            },
+        );
+    }
+
+    Future<void> insertFood(Food food) async {
+        await _database?.insert(
+            'food',
+            {
+                'name': food.name,
+                'expirationDate': food.expirationDate.toIso8601String(),
+            },
+        );
+    }
+
+    Future<List<Food>> getAllFoods() async {
+        final List<Map<String, Object?>> rows = await _database!.query('food');
+        return rows.map((row) {
+            return Food(
+                id: row['id'] as int,
+                name: row['name'] as String,
+                expirationDate: DateTime.parse(row['expirationDate'] as String)
+            );
+        }).toList();
+    }
+
+    Future<void> deleteFood(int id) async {
+        await _database?.delete(
+            'food', 
+            where: 'id = ?',
+            whereArgs: [id]
+        );
+    }
 }
+
