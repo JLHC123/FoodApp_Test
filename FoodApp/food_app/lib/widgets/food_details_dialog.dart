af06cfd4fe0f83943d42005c8d9e944f6f706df8
JLHC123 <jlhc3751@gmail.com>
2026-07-14 22:05:04 -0700

    you can now use the links and it will take you to the page. Needed to install url_launcher and also fix issues with flutter doctor (missing some android SDK and Desktop development things)

--- FoodApp/food_app/lib/widgets/food_details_dialog.dart
+++ FoodApp/food_app/lib/widgets/food_details_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/food.dart';
 import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FoodDetailsDialog extends StatelessWidget {
   final Food food;
@@ -12,17 +13,48 @@ class FoodDetailsDialog extends StatelessWidget {
 
   @override 
   Widget build(BuildContext context) {
+    final expirationDate = DateFormat('yyyy-MM-dd').format(food.expirationDate);
+    final foodName = Uri.encodeComponent(food.name);
+    final foodUrl = Uri.parse('https://www.google.com/search?q=$foodName');
+    final recipeUrl = Uri.parse('https://www.google.com/search?q=$foodName+recipes');
     return AlertDialog(
       title: Text(food.name),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Expiration Date: ${DateFormat('yyyy-MM-dd').format(food.expirationDate)}'),
-          Text('Useful Links: '),
-          Text('https://www.google.com/search?q=${food.name}'),
-          Text('https://www.google.com/search?q=${food.name}+recipes'),
-        ]
+          Text('Expiration Date: $expirationDate'),
+          const SizedBox(height: 8),
+          InkWell(
+            onTap: () async {
+              if (await canLaunchUrl(foodUrl)) {
+                await launchUrl(foodUrl);
+              }
+            },
+            child: const Text(
+              'Search for more information',
+              style: TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          InkWell(
+            onTap: () async {
+              if (await canLaunchUrl(recipeUrl)) {
+                await launchUrl(recipeUrl);
+              }
+            },
+            child: const Text(
+              'Search for recipes',
+              style: TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
+        ],
       )
     );
   }
